6da411caeefc20f88b04fe993121d0c0a2e0d8e1
Matthias Mitterreiter <matthias@mitterreiter.de>
2024-08-29 23:02:41 +0700

    Add actions for flake8 mypy and pytest

---
+++ build/lib/pwgen/charsets.py
@@ -0,0 +1,13 @@
+UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWER = 'abcdefghijklmnopqrstuvwxyz'
+SYMBOL = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+DIGITS = '0123456789'
+ALL = UPPER + LOWER + SYMBOL + DIGITS
+
+CHARMAP = {
+    "upper": UPPER,
+    "lower": LOWER,
+    "symbol": SYMBOL,
+    "digit": DIGITS,
+    "all": ALL
+}
