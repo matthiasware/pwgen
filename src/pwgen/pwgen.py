3d8ee2c86979b40d300a011f0e1fd614ef0ed455
Matthias Mitterreiter <matthias@mitterreiter.de>
2024-08-31 17:11:30 +0700

    Add CLI

--- src/pwgen/pwgen.py
+++ src/pwgen/pwgen.py
@@ -13,18 +13,12 @@ class CharsetMinSamples:
 
 
 def generate_password(length: int, charsets: list[CharsetMinSamples]) -> str:
-    # validate if password of given length is creatable
-    fixed_length = sum(cs.n_samples for cs in charsets)
-    if fixed_length > length:
-        raise ValueError((f"Cannot create password of length '{length}'"
-                          "since the amount of specified mandadorty"
-                          "characters is {fixed_length}!"))
-
     password = []
     for charset in charsets:
         password += random.choices(charset.charset, k=charset.n_samples)
 
-    all_characters = "".join(charset.charset for charset in charsets)
-    password += random.choices(all_characters, k=length - len(password))
+    if len(password) < length:
+        all_characters = "".join(charset.charset for charset in charsets)
+        password += random.choices(all_characters, k=length - len(password))
     random.shuffle(password)
     return "".join(password)
